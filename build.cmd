ed66e5572e044613bceac5f0c4734058ca766be2
roryplaire <rory@esha.com>
2017-12-09 03:00:20 -0800

    Fixing build scripts

--- build.cmd
+++
@@ -1,3 +0,0 @@
-rem install: choco install nuget.commandline
-nuget restore source\NHibernate.AspNet.Identity.sln
-"%PROGRAMFILES(x86)%\MSBuild\12.0\bin\msbuild.exe" "source\NHibernate.AspNet.Identity.sln" /t:Clean;Rebuild /p:Configuration=Release
